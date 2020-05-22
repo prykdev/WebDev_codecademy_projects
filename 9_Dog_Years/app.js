502a060ab2a7cf2682b077f04538e124ce67fe64
priyanka prasad <59612128+P-riyanka-prasad@users.noreply.github.com>
2020-05-23 00:42:27 +0530

    dogs year

---
+++ 9_Dog_Years/app.js
@@ -0,0 +1,11 @@
+const myAge=18;
+let earlyYears =2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+myAgeInDogYears=earlyYears +laterYears 
+myName='Priyanka prasad'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+myAge*= 10;
